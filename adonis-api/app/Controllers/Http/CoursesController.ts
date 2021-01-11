50b3bdaa586a9e26084532786c32f78d1f9797be
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-01-11 17:53:33 -0300

    Delete course

--- adonis-api/app/Controllers/Http/CoursesController.ts
+++ adonis-api/app/Controllers/Http/CoursesController.ts
@@ -1,7 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Course from 'App/Models/Course'
 
 export default class CoursesController {
   public async index ({}: HttpContextContract) {
+    return await Course.all();
   }
 
   public async store ({}: HttpContextContract) {
@@ -13,6 +15,8 @@ export default class CoursesController {
   public async update ({}: HttpContextContract) {
   }
 
-  public async destroy ({}: HttpContextContract) {
+  public async destroy ({ params }: HttpContextContract) {
+    const course = await Course.findOrFail(params.id)
+    await course.delete()
   }
 }
